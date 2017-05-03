291a1314cdf95579e79d18e1e1ded5d4db1cde05
Randy Tarampi <rt@randytarampi.ca>
2017-05-03 01:25:05 -0700

    Need to export `downloadFileFromBucket`.
    Might as well do `downloadFileFromHttp` as well.

--- src/util/index.js
+++ src/util/index.js
@@ -1,7 +1,11 @@
+var downloadFileFromBucket = require('./downloadFileFromBucket');
+var downloadFileFromHttp = require('./downloadFileFromHttp');
 var downloadFileFromUrl = require('./downloadFileFromUrl');
 var notifySns = require('./notifySns');
 
 module.exports = {
+  downloadFileFromBucket: downloadFileFromBucket,
+  downloadFileFromHttp: downloadFileFromHttp,
   downloadFileFromUrl: downloadFileFromUrl,
   notifySns: notifySns
 };
